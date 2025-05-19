b6213cf7992dd7c50f1688551d8896d9895e23a9
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-19 11:34:37 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "cloudtrail_s3_bucket" {
+  description = "S3 bucket name for CloudTrail logs"
+  type        = string
+}
+
+variable "alert_email" {
+  description = "Email to receive SNS alerts"
+  type        = string
+}
