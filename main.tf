d59a1da5947dc386a1a8b730e463c47aa4258413
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-19 11:35:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,45 @@
+resource "aws_cloudtrail" "trail" {
+  name                          = "${var.name_prefix}-cloudtrail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.bucket
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_role.arn
+}
+
+resource "aws_cloudwatch_log_group" "cloudtrail" {
+  name              = "/aws/cloudtrail/${var.name_prefix}"
+  retention_in_days = 7
+}
+
+resource "aws_iam_role" "cloudtrail_role" {
+  name = "${var.name_prefix}-cloudtrail-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "cloudtrail.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "cloudtrail_policy" {
+  name = "${var.name_prefix}-cloudtrail-policy"
+  role = aws_iam_role.cloudtrail_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      Resource = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
+    }]
+  })
+}
 
