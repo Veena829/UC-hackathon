12b0bb87a50f3d4a470fb91dae46c781ade1144d
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-19 12:27:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
-  #default     = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "name_prefix" {
